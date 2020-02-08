c3e4c24dccdaee9585a9ef08cec0ef4c135a97e8
iamzlatan <zlatan@gmail.com>
2020-02-08 19:30:02 +0200

    Updates

---
+++ precache-manifest.c0e050719dd7450ede0c70b0f5f0a142.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26a72defdb9b60a148a74153fce449e6",
+    "url": "/laser/index.html"
+  },
+  {
+    "revision": "e1623d7f0c2eb1a4031f",
+    "url": "/laser/static/css/main.93321299.chunk.css"
+  },
+  {
+    "revision": "7fcc01d78519add9180b",
+    "url": "/laser/static/js/2.f450edf1.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/laser/static/js/2.f450edf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1623d7f0c2eb1a4031f",
+    "url": "/laser/static/js/main.482bdeca.chunk.js"
+  },
+  {
+    "revision": "6b48b4a2ccd7f21dd59a",
+    "url": "/laser/static/js/runtime-main.459ebb9c.js"
+  }
+]);
